7eae221f8e48c87caf3ef90e87fb3adcf3b5ec4b
Mateusz Kozakiewicz <mateusz.kozakiewicz@skygate.io>
2020-04-01 02:55:42 +0200

    add settings tab

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-const { app, BrowserWindow, Menu } = require("electron");
+require("electron-reload")(__dirname);
+const {
+  app,
+  BrowserWindow,
+  Menu,
+  MenuItem,
+  ipcMain,
+  globalShortcut
+} = require("electron");
 const Store = require("electron-store");
 const config = require("./config/env.json");
 
@@ -18,20 +26,24 @@ function createWindow() {
     ...store.get("winBounds")
   });
 
-  if (store.get("maximized")) mainWindow.maximize();
-  if (store.get("was_fullscreen")) mainWindow.setFullScreen(true);
+  if (store.get("isMaximized")) mainWindow.maximize();
+  if (store.get("isFullscreen")) mainWindow.setFullScreen(true);
   if (config.debug) mainWindow.webContents.openDevTools();
 
-  mainWindow.loadFile("index.html");
+  mainWindow.loadFile("./views/MainWindow/MainWindow.html");
   mainWindow.webContents.on("dom-ready", () => {
-    mainWindow.webContents.send("emby-url", config.embyUrl);
-    if (!config.debug) mainWindow.webContents.send("hide-menu");
+    if (store.get("settings", {}).embyUrl)
+      mainWindow.webContents.send(
+        "emby-url",
+        store.get("settings", {}).embyUrl
+      );
+    else mainWindow.webContents.send("settings-open");
   });
 
   mainWindow.on("close", () => {
     store.set("winBounds", mainWindow.getBounds());
-    store.set("maximized", mainWindow.isMaximized());
-    store.set("was_fullscreen", mainWindow.isFullScreen());
+    store.set("isMaximized", mainWindow.isMaximized());
+    store.set("isFullscreen", mainWindow.isFullScreen());
   });
 
   mainWindow.on("enter-full-screen", () => {
@@ -40,6 +52,41 @@ function createWindow() {
 
   mainWindow.on("leave-full-screen", () => {
     mainWindow.webContents.send("title-bar-visible", true);
+  });
+
+  ipcMain.on("settings-save", function(event, settings) {
+    store.set("settings", settings);
+    if (settings.embyUrl) mainWindow.webContents.send("emby-url", settings);
+    mainWindow.reload();
+  });
+
+  const menu = new Menu();
+  menu.append(
+    new MenuItem({
+      label: "Emby",
+      click() {
+        mainWindow.webContents.send("emby-open");
+      }
+    })
+  );
+  menu.append(
+    new MenuItem({
+      label: "Settings",
+      click() {
+        mainWindow.webContents.send("settings-open");
+      }
+    })
+  );
+  Menu.setApplicationMenu(menu);
+
+  globalShortcut.register("F5", function() {
+    mainWindow.reload();
+  });
+  globalShortcut.register("CommandOrControl+R", function() {
+    mainWindow.reload();
+  });
+  globalShortcut.register("F11", function() {
+    mainWindow.setFullScreen(!mainWindow.isFullScreen());
   });
 }
 
---
+++ views/MainWindow/MainWindow.js
@@ -0,0 +1,65 @@
+const customTitlebar = require("custom-electron-titlebar");
+const { ipcRenderer } = require("electron");
+const Store = require("electron-store");
+
+const store = new Store();
+
+function openEmby() {
+  document.getElementById("emby-instance").style.display = "flex";
+  document.getElementById("settings").style.display = "none";
+}
+
+function openSettings() {
+  document.getElementById("emby-instance").style.display = "none";
+  document.getElementById("settings").style.display = "block";
+  document.getElementById("settings-input-emby-url").value =
+    store.get("settings", {}).embyUrl || "";
+}
+
+ipcRenderer.on("emby-open", function(event) {
+  openEmby();
+});
+
+ipcRenderer.on("settings-open", function(event) {
+  openSettings();
+});
+
+ipcRenderer.on("emby-url", function(event, embyUrl) {
+  document.getElementById("emby-instance").src = embyUrl;
+});
+
+ipcRenderer.on("title-bar-visible", function(event, isVisible) {
+  Array.from(
+    document.getElementsByClassName("container-after-titlebar")
+  ).forEach(function(element) {
+    if (isVisible) element.style.top = "30px";
+    else element.style.top = "0px";
+  });
+  Array.from(document.getElementsByClassName("titlebar windows")).forEach(
+    function(element) {
+      if (isVisible) element.style.height = "30px";
+      else element.style.height = "0px";
+    }
+  );
+  Array.from(document.getElementsByClassName("menubar")).forEach(function(
+    element
+  ) {
+    if (isVisible) element.style.display = "flex";
+    else element.style.display = "none";
+  });
+});
+
+const titlebar = new customTitlebar.Titlebar({
+  backgroundColor: customTitlebar.Color.fromHex("#141414")
+});
+
+document.getElementById("settings-save").onclick = function() {
+  ipcRenderer.send("settings-save", {
+    embyUrl: document.getElementById("settings-input-emby-url").value
+  });
+  openEmby();
+};
+
+document.getElementById("settings-cancel").onclick = function() {
+  openEmby();
+};
